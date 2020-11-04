33169756c714f6d87fba1e71d6bd2e972a1317a8
Kaz NAGAMINE <7134481+kaznag@users.noreply.github.com>
2020-11-05 08:10:46 +0900

    Change maximize button blur color

--- src/renderer/components/TitleBar.tsx
+++ src/renderer/components/TitleBar.tsx
@@ -16,7 +16,7 @@ const TitleBar: React.FC<TitleBarProps> = props => {
     <div className="title-bar">
       <div className="drag-region"></div>
       <div className={'close-button' + (!props.isFocused ? ' blur' : '')} onClick={props.onCloseButtonClick}></div>
-      <div className={(props.isMaximized ? 'title-bar button restore' : 'maximize-button')} onClick={props.onMaximizeRestoreButtonClick}></div>
+      <div className={(props.isMaximized ? 'title-bar button restore' : 'maximize-button') + (!props.isFocused ? ' blur' : '')} onClick={props.onMaximizeRestoreButtonClick}></div>
       <div className="title-bar button minimize" onClick={props.onMinimizeButtonClick}></div>
       <div className={'window-title' + (!props.isFocused ? ' blur' : '')}>{props.windowTitle}</div>
     </div>

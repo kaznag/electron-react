f3c727403d52031d2c2fdfacd2d829ec02335dcc
Kaz NAGAMINE <7134481+kaznag@users.noreply.github.com>
2022-02-26 15:05:41 +0900

    Improve Counter container

--- src/renderer/containers/CounterContainer.tsx
+++ src/renderer/containers/CounterContainer.tsx
@@ -1,23 +1,25 @@
-import React from 'react';
+import React, { useCallback } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import Counter from '../components/Counter';
 import { countSelector } from '../store/counter/counterSelector';
 import { decrement, increment, reset } from '../store/counter/counterSlice';
 
-type Props = {};
-
-const CounterContainer: React.FC<Props> = () => {
+function CounterContainer(): JSX.Element {
   const count = useSelector(countSelector);
   const dispatch = useDispatch();
+
+  const incrementCount = useCallback((count: number) => dispatch(increment(count)), [dispatch]);
+  const decrementCount = useCallback((count: number) => dispatch(decrement(count)), [dispatch]);
+  const resetCount = useCallback(() => dispatch(reset()), [dispatch]);
 
   return (
     <Counter
       count={count}
-      onIncrementClick={(count: number) => dispatch(increment(count))}
-      onDecrementClick={(count: number) => dispatch(decrement(count))}
-      onResetClick={() => dispatch(reset())}
+      onIncrementClick={(count: number) => incrementCount(count)}
+      onDecrementClick={(count: number) => decrementCount(count)}
+      onResetClick={() => resetCount()}
     ></Counter>
   );
-};
+}
 
 export default CounterContainer;
